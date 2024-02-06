adab80b2a33fa4d5c6c38679a597e3fb7e2622f9
Hexygo <61022708+Hexygo@users.noreply.github.com>
2024-02-06 23:35:39 +0100

    Added frontend Evaluation unit test

--- backend/tests/frontend_unit_test.py
+++ backend/tests/frontend_unit_test.py
@@ -128,3 +128,55 @@ def test_can_run_trough_form_recommendation(start_container, page:Page): # Verif
     #download = download_info.value
 
     container.stop()
+
+def test_can_run_trough_form_evaluation(start_container, page:Page):
+    container = start_container.start()
+    delay = waiting_utils.wait_for_logs(container, 'Accepting connections')
+
+    page.goto("http://127.0.0.1:3000/evaluation")
+
+    page.get_by_role("button", name="start").click()
+
+    with page.expect_file_chooser() as fc_info:
+        page.get_by_text("Select a .tsv file").first.click()
+    fc_info.value.set_files(os.path.join(os.path.dirname(__file__), 'data', 'sample_train.tsv'))
+    with page.expect_file_chooser() as fc_info:
+        page.get_by_text("Select a .tsv file").nth(1).click()
+    fc_info.value.set_files(os.path.join(os.path.dirname(__file__), 'data', 'sample_test.tsv'))
+    with page.expect_file_chooser() as fc_info:
+        page.get_by_text("Select a .tsv file").last.click()
+    fc_info.value.set_files(os.path.join(os.path.dirname(__file__), 'data', 'sample_recs.tsv'))
+    page.get_by_role("button", name="Next").click()
+
+    page.get_by_label("Relevance Treshold").click()
+    page.get_by_label("Relevance Treshold").fill("1")
+    page.get_by_label("Top K").click()
+    page.get_by_label("Top K").fill("1")
+    page.get_by_label("Paired tTest").check()
+    page.get_by_label("Paired tTest").uncheck()
+    page.get_by_label("Wilicoxon Test").check()
+    page.get_by_label("Wilicoxon Test").uncheck()
+    page.get_by_role("button", name="Next").click()
+
+    page.get_by_role("button", name="Add Cutoffs").click()
+    page.get_by_label("cutoff 1").click()
+    page.get_by_label("cutoff 1").fill("1")
+    page.get_by_role("button", name="Add Cutoffs").click()
+    page.locator("li").filter(has_text="cutoff 2 *cutoff 2 *").get_by_role("button").click()
+    page.get_by_text("Set a number of cutoffs Add").click()
+    page.get_by_role("button", name="Next").click()
+
+    page.get_by_label("AUC", exact=True).check()
+    page.get_by_label("GAUC").check()
+    page.get_by_label("GAUC").uncheck()
+    page.get_by_role("button", name="Next").click()
+
+    page.get_by_label("DSC").check()
+    page.get_by_label("beta").click()
+    page.get_by_label("beta").fill("1")
+    page.get_by_label("ExtendedF1").check()
+    page.get_by_label("DSC").uncheck()
+
+    page.get_by_role("button", name="Evaluate with this options").click()
+
+    container.stop()
